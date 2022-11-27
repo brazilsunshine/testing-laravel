4c3152aede1b2fb69fb95f520c46311dda726590
Sthefane <sthefanesoliveiraa@outlook.com>
2022-11-27 22:27:04 +0000

    testing request validation

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,8 +1,6 @@
 import _ from 'lodash';
 window._ = _;
 
-import 'bootstrap';
-
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -11,8 +9,16 @@ import 'bootstrap';
 
 import axios from 'axios';
 window.axios = axios;
-
+window.axios.defaults.withCredentials = true;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
---
+++ resources/js/components/RootContainer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>
+            test root container
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "RootContainer"
+}
+</script>
+
+<style scoped>
+
+</style>
